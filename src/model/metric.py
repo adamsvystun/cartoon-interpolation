c12d741425483434eae8e08f8f7cf8358f8bd005
Adam Svystun <adam.svystun@gmail.com>
2019-11-16 21:09:59 -0500

    Update ssim metric to use skimage

--- src/model/metric.py
+++ src/model/metric.py
@@ -1,9 +1,6 @@
-from math import exp
-
 import numpy as np
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
+from skimage.metrics import structural_similarity
 
 
 def accuracy(output, target):
@@ -37,51 +34,11 @@ def psnr(outputs, targets):
     return value / len(outputs)
 
 
-def _gaussian(window_size, sigma):
-    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
-    return gauss / gauss.sum()
-
-
-def _create_window(window_size, channel):
-    _1D_window = _gaussian(window_size, 1.5).unsqueeze(1)
-    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
-    return window
-
-
-def _ssim(img1, img2, window, window_size, channel, size_average=True):
-    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
-    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1 * mu2
-
-    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
-    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
-    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
-
-    c1 = 0.01 ** 2
-    c2 = 0.03 ** 2
-
-    ssim_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2))
-
-    if size_average:
-        return ssim_map.mean()
-    else:
-        return ssim_map.mean(1).mean(1).mean(1)
-
-
-def ssim(outputs, targets, window_size=11, size_average=True):
-    (_, channel, _, _) = outputs[0].size()
-    window = _create_window(window_size, channel)
-
-    if outputs[0].is_cuda:
-        window = window.cuda(outputs[0].get_device())
-    window = window.type_as(outputs[0])
-
-    ssim = 0
+def ssim(outputs, targets):
+    value = 0
     for output, target in zip(outputs, targets):
-        ssim += _ssim(output, target, window, window_size, channel, size_average)
-
-    return ssim / len(outputs)
+        with torch.no_grad():
+            original = target.cpu().detach().numpy()
+            compared = output.cpu().detach().numpy()
+            value += structural_similarity(original, compared)
+    return value / len(outputs)
