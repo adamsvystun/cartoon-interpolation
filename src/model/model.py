cd032983a57b6643d80f9a3dc8b608b78a86ff70
Michał Giemza <m.giemza@sigmoidal.io>
2019-12-01 05:13:56 +0100

    Add Flow CCNN model

--- src/model/model.py
+++ src/model/model.py
@@ -1,9 +1,9 @@
 import torch
 
 from src.model.sepconv_network import SepConvNet
-from src.model.ccnn.model import CCNN, DoubleStreamedCCNN
+from src.model.ccnn.model import CCNN, DoubleStreamedCCNN, FlowCCNN
 
-__all__ = ['CycleCCNN', 'CycleSepConv', 'DoubleStreamedCycleCCNN']
+__all__ = ['CycleCCNN', 'CycleSepConv', 'DoubleStreamedCycleCCNN', 'FlowCycleCCNN']
 
 
 class CycleCCNN(torch.nn.Module):
@@ -47,6 +47,26 @@ class DoubleStreamedCycleCCNN(torch.nn.Module):
     def __init__(self):
         super(DoubleStreamedCycleCCNN, self).__init__()
         self.backbone = DoubleStreamedCCNN()
+
+    def forward_single(self, frame0, frame2):
+        frame1_pred = self.backbone(frame0, frame2)
+        return frame1_pred
+
+    def forward(self, frame0, frame2, frame4):
+        frame1_pred = self.backbone(frame0, frame2)
+        frame3_pred = self.backbone(frame2, frame4)
+        frame2_pred = self.backbone(frame1_pred, frame3_pred)
+        return frame1_pred, frame2_pred, frame3_pred
+
+    def predict(self, frame0, frame2):
+        return self.backbone(frame0, frame2)
+
+
+class FlowCycleCCNN(torch.nn.Module):
+
+    def __init__(self):
+        super(FlowCycleCCNN, self).__init__()
+        self.backbone = FlowCCNN()
 
     def forward_single(self, frame0, frame2):
         frame1_pred = self.backbone(frame0, frame2)
