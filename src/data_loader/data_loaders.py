64f87d684410dc696732e6f98b9ebfe1e901b248
Adam Svystun <adam.svystun@gmail.com>
2019-11-30 20:08:07 -0500

    Add pretrain data loader

--- src/data_loader/data_loaders.py
+++ src/data_loader/data_loaders.py
@@ -1,5 +1,5 @@
 from src.data_loader.base import BaseDataLoader
-from src.dataset import DD40Dataset
+from src.dataset import DD40Dataset, PretrainDataLoader
 
 
 class DD40DataLoader(BaseDataLoader):
@@ -10,4 +10,15 @@ class DD40DataLoader(BaseDataLoader):
                  validation_split=0.0, num_workers=1, training=True):
         self.data_dir = data_dir
         self.dataset = DD40Dataset(self.data_dir, train=training, dataset_file=dataset_csv, return_paths=return_paths)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class PretrainDataLoader(BaseDataLoader):
+    """
+    PretrainDataLoader Data Loader
+    """
+    def __init__(self, data_dir, batch_size, dataset_csv, shuffle=True, return_paths=False,
+                 validation_split=0.0, num_workers=1, training=True):
+        self.data_dir = data_dir
+        self.dataset = PretrainDataLoader(self.data_dir, train=training, dataset_file=dataset_csv, return_paths=return_paths)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
